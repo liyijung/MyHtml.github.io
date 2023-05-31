452feec590fe49a30787f257c6819359774ed2a7
liyijung <132187351+liyijung@users.noreply.github.com>
2023-05-31 23:43:34 +0800

    version1

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function sendRequest() {
 
   $.ajax({
     type: "POST",
-    url: "/sendrequest",
+    url: "https://ac31-2001-b011-5c00-1a21-60eb-9439-a5ff-3b66.ngrok-free.app/sendrequest",
     data: { function_options: function_options, inputText: inputText },
     success: function(response) {
       document.getElementById("outputText").value = response.result;
     },
     error: function() {
-      alert("请求失败");
+      alert("請求失敗");
     }
   });
 }
