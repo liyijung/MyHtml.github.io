5a1b40f9910dccdc734aa77c10aac0632f7b2f80
liyijung <132187351+liyijung@users.noreply.github.com>
2023-06-01 00:04:09 +0800

    version1

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function sendRequest() {
     url: "https://ac31-2001-b011-5c00-1a21-60eb-9439-a5ff-3b66.ngrok-free.app/sendrequest",
     data: { function_options: function_options, inputText: inputText },
     success: function(response) {
-      document.getElementById("outputText").value = response.result;
+      document.getElementById("outputText").innerText = response.result;
     },
     error: function() {
       alert("請求失敗");
