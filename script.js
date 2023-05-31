5d1a94172af78d7737195258ffe79d6f92f7a8c4
liyijung <132187351+liyijung@users.noreply.github.com>
2023-06-01 01:50:41 +0800

    version1

---
+++ script.js
@@ -0,0 +1,13 @@
+function sendRequest() {
+  var function_options = document.getElementById("function_options").value;
+  var inputText = document.getElementById("inputText").value;
+
+  $.ajax({
+    type: "POST",
+    url: "https://ac31-2001-b011-5c00-1a21-60eb-9439-a5ff-3b66.ngrok-free.app/sendrequest",
+    data: { function_options: function_options, inputText: inputText },
+	success: function(response) {
+	  var result = response.result;
+	  document.getElementById("outputText").innerText = result;},
+  error: function() {
+    alert("請求失敗");}});}
