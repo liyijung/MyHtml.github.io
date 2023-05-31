71a4d37e6480ca891943c9ab4dc9ba81cefa0a1f
liyijung <132187351+liyijung@users.noreply.github.com>
2023-05-31 23:18:56 +0800

    version1

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function sendRequest() {
 
   $.ajax({
     type: "POST",
-    url: "https://ac31-2001-b011-5c00-1a21-60eb-9439-a5ff-3b66.ngrok-free.app",
+    url: "https://ac31-2001-b011-5c00-1a21-60eb-9439-a5ff-3b66.ngrok-free.app/sendrequest",
     data: { function_options: function_options, inputText: inputText },
     success: function(response) {
       document.getElementById("outputText").value = response.result;
