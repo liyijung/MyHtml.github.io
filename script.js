284e39ef92fba4214b1a09dc9ce60bb06f9b7b5d
liyijung <132187351+liyijung@users.noreply.github.com>
2023-05-31 22:44:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function sendRequest() {
+  var function_options = document.getElementById("function_options").value;
+  var inputText = document.getElementById("inputText").value;
+
+  $.ajax({
+    type: "POST",
+    url: "https://ac31-2001-b011-5c00-1a21-60eb-9439-a5ff-3b66.ngrok-free.app",
+    data: { function_options: function_options, inputText: inputText },
+    success: function(response) {
+      document.getElementById("outputText").value = response.result;
+    },
+    error: function() {
+      alert("请求失败");
+    }
+  });
+}
